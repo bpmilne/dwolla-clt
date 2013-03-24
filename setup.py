a86c9ed9114d6318048cae219b4e5589e3291a15
Michael Schonfeld <michael@dwolla.com>
2013-03-24 11:07:25 -0400

    bump version

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 setup(
     name='dwolla-clt',
     license='MIT',
-    version='1.2.1',
+    version='1.2.2',
     install_requires=[
         'dwolla>=1.4.8',
         'argparse>=1.2.1'
