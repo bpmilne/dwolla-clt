af2ddde96171e9a3d925168d89679045a5bb6762
Michael Schonfeld <michael@dwolla.com>
2013-03-24 12:20:19 -0400

    add phone destination type

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 setup(
     name='dwolla-clt',
     license='MIT',
-    version='1.2.3',
+    version='1.2.4',
     install_requires=[
         'dwolla>=1.4.8',
         'argparse>=1.2.1'
